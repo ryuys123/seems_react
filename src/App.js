b1ad86a4fc97c3afeeb9b1b69e45054fd70a6371
seonhye00 <ssh8494@gmail.com>
2025-07-08 18:12:54 +0900

    공지사항 목록 조회

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 // 사용할 컴포넌트 불러오기 : 모든 페이지에 적용됨
-import Footer from './components/common/Footer';
+import Footer from "./components/common/Footer";
 
 // 별도로 작성된 라우터 등록 설정 파일을 불러오기함
-import AppRouter from './routers/router';
+import AppRouter from "./routers/router";
+import NoticeListPage from "./pages/notice/NoticeListPage";
 
 function App() {
   return (
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,21 +1,25 @@
 // src/routers/router.js
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Login from '../pages/Login';
+import Login from "../pages/Login";
 
 // 기능별로 작성한 라우터를 불러오기
-import userRoutes from './userRoutes';
+import userRoutes from "./userRoutes";
+import NoticeListPage from "../pages/notice/NoticeListPage";
 
 const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Login />} />
-            {/* {userRoutes} */}
-            {userRoutes.map((route => <Route key={route.path} 
-            path={route.path} element={route.element} />))}
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      {/* {userRoutes} */}
+      {userRoutes.map((route) => (
+        <Route key={route.path} path={route.path} element={route.element} />
+      ))}
+      {/* {noticeRoutes} */}
+      <Route path="/notice" element={<NoticeListPage />} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
