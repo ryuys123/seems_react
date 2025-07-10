bc02dc6084e3c0aef528e7eebe1fb0b6e8e28552
LINUS0510 <jjs2245510@naver.com>
2025-07-10 17:36:26 +0900

    정진석 - 퀘스트 / 컨텐츠 리엑트 프론트앤드

---
+++ src/routers/contentRoutes.js
@@ -0,0 +1,10 @@
+import ContentPage from "../pages/content/ContentPage";
+
+const contentRoutes = [
+  {
+    path: "/content",
+    element: <ContentPage />
+  }
+];
+
+export default contentRoutes;
