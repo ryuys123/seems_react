d2aec1595dedf7d909befa5c59910146df526eff
seonhye00 <ssh8494@gmail.com>
2025-07-10 12:33:04 +0900

    라우터 합침-250710

--- src/routers/router.js
+++ src/routers/router.js
@@ -6,9 +6,12 @@ import Login from "../pages/Login";
 
 // 기능별로 작성한 라우터를 불러오기
 import userRoutes from "./userRoutes";
-import NoticeListPage from "../pages/notice/NoticeListPage";
+import NoticeListPage from "./noticeRoutes";
 import adminRoutes from "./adminRoutes";
 import testRoutes from "./testRoutes";
+import noticeRoutes from "./noticeRoutes";
+import questRoutes from "./questRoutes";
+import contentRoutes from "./contentRoutes";
 
 const AppRouter = () => {
   return (
@@ -18,15 +21,19 @@ const AppRouter = () => {
       {userRoutes.map((route) => (
         <Route key={route.path} path={route.path} element={route.element} />
       ))}
-      {/* {noticeRoutes} */}
-      <Route path="/notice" element={<NoticeListPage />} />
-      {adminRoutes.map((route) => (
-        <Route key={route.path} path={route.path} element={route.element} />
-      ))}
+      {noticeRoutes}
       {/* {testRoutes} */}
       {/* {testRoutes.map((route) => (
         <Route key={route.path} path={route.path} element={route.element} />
       ))} */}
+      {/* Quest Routes */}
+      {questRoutes.map((route) => (
+        <Route key={route.path} path={route.path} element={route.element} />
+      ))}
+      {/* Content Routes */}
+      {contentRoutes.map((route) => (
+        <Route key={route.path} path={route.path} element={route.element} />
+      ))}
     </Routes>
   );
 };
