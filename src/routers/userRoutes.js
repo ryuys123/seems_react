18c7b4b1e6cb4907bebf7f30e8af335c910877d6
js <127711639+shinjaesang@users.noreply.github.com>
2025-07-11 10:04:41 +0900

    Add files via upload

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -8,6 +8,10 @@ import PwFindIdPage from '../pages/user/PwFindIdPage';
 import PwFindEmailPage from '../pages/user/PwFindEmailPage';
 import UserProfilePage from '../pages/user/UserProfilePage';
 import UserDashboard from '../pages/user/UserDashboard';
+import UserFormPage from '../pages/user/UserFormPage';
+import UserPreferencePage from '../pages/user/UserPreferencePage';
+import UserSimulationSettingsPage from '../pages/user/UserSimulationSettingsPage';
+import UserDeletePage from '../pages/user/UserDeletePage';
 import IdFindSelectPage from '../pages/user/IdFindSelectPage';
 import IdFindPhonePage from '../pages/user/IdFindPhonePage';
 import IdFindEmailPage from '../pages/user/IdFindEmailPage';
@@ -48,7 +52,23 @@ const userRoutes = [
   {
     path: '/idfindemail',
     element: <IdFindEmailPage />
-  }
+  },
+  {
+    path: '/userform',
+    element: <UserFormPage />,
+  },
+  {
+    path: '/user/preferences',
+    element: <UserPreferencePage />,
+  },
+  {
+    path: '/user/simulation-settings',
+    element: <UserSimulationSettingsPage />,
+  },
+  {
+    path: '/user/delete',
+    element: <UserDeletePage />,
+  },
 ];
 
 export default userRoutes;
